035d7a196db8b946ef0de11fde1f552282e75ba2
Ryan Youngblood <rryb77@gmail.com>
2021-05-02 14:22:08 -0500

    fleshed out the modal for Q and A's, need to work on backend to link this up

--- FlashcardFight/client/src/components/flashcardsets/FlashCardEdit.js
+++ FlashcardFight/client/src/components/flashcardsets/FlashCardEdit.js
@@ -22,11 +22,13 @@ import {
 
 const FlashCardEdit = () => {
 
+    // Imported contexts
     const { getFlashcardSetWithQandA, updateSet } = useContext(FlashCardSetContext);
     const { categories, getAllCategories, setCategories } = useContext(CategoryContext);
     const { difficulties, getAllDifficulties, setDifficulties } = useContext(DifficultyContext);
     const [flashcardSet, setFlashcardSet] = useState({});
 
+    // Imported react router hooks
     const history = useHistory();  
     const {id} = useParams();
 
@@ -34,11 +36,24 @@ const FlashCardEdit = () => {
     const [flashcardModal, setFlashcardModal] = useState(false);
     const toggleFlashcardModal = () => setFlashcardModal(!flashcardModal);
 
-    // flashcard set details form field states
+    // flashcard set form field states
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [category, setCategory] = useState("");
     const [difficulty, setDifficulty] = useState("");
+
+    // question and answer modal state
+    const [qAndAModal, setQAndAModal] = useState(false);
+    const toggleQAndAModal = () => setQAndAModal(!qAndAModal);
+
+    // question and answers field states
+    const [userQuestion, setUserQuestion] = useState("");
+    const [correctAnswer, setCorrectAnswer] = useState("");
+    const [wrongAnswer1, setWrongAnswer1] = useState("");
+    const [wrongAnswer2, setWrongAnswer2] = useState("");
+    const [wrongAnswer3, setWrongAnswer3] = useState("");
+
+    
 
     // Initial load
     useEffect(() => {
@@ -50,7 +65,7 @@ const FlashCardEdit = () => {
             .then(setDifficulties)
     },[])
 
-    // Once the post has been set in state, update the form with previous post info
+    // Once the flashcard has been set in state, update the form with current flashcard info from DB
     useEffect(() => {
 
         setTitle(flashcardSet.title)
@@ -59,7 +74,9 @@ const FlashCardEdit = () => {
         setDifficulty(flashcardSet.difficultyId)
     }, [flashcardSet])
 
-    const saveEdit = () => {
+    
+    // Save the flashcard edits that were made
+    const saveFlashcardEdit = () => {
         const updatedSet = {
             ...flashcardSet
         }
@@ -75,6 +92,12 @@ const FlashCardEdit = () => {
         toggleFlashcardModal()
     }
 
+    // Save Q and A edits
+    const saveQAndAdEdit = () => {
+        
+    }
+
+    // If flashcardSet hasn't mounted yet then return null
     if(flashcardSet.id === undefined)
     {
         return null;
@@ -83,6 +106,8 @@ const FlashCardEdit = () => {
     return (
         <div className="container pt-4">
             <div className="justify-content-center">
+                
+                {/* Flashcard details */}
                 <Container>
                     <div>Title: {flashcardSet?.title}</div>
                     <div>Description: {flashcardSet?.description}</div>
@@ -92,6 +117,7 @@ const FlashCardEdit = () => {
                     <br></br><br></br>
                 </Container>
 
+                {/* List of questions */}
                 <Container>
                     {
                         flashcardSet.questions.map(q => {
@@ -104,8 +130,70 @@ const FlashCardEdit = () => {
                     }
                 </Container>
             </div>
+                
+                {/* Modal for flashcard details */}
                 <Modal isOpen={flashcardModal} toggle={toggleFlashcardModal} className="nes-dialog">
                     <ModalHeader toggle={toggleFlashcardModal}>Flashcard Details Edit</ModalHeader>
+                    <ModalBody>
+                        <Form>
+                            <FormGroup>
+                                <Label for="title">Title</Label>
+                                <Input id="title" onChange={(e) => setTitle(e.target.value)} value={title} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="title">Description</Label>
+                                <Input type="textarea"
+                                id="description"
+                                onChange={(e) => setDescription(e.target.value)}
+                                value={description}
+                                rows="5"
+                            />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="category">Category</Label><br></br>
+                                <div className="nes-select">
+                                    <select required id="category" onChange={(e) => setCategory(e.target.value)}>
+                                        <option value={`${flashcardSet.categoryId}`} disabled selected hidden>{flashcardSet.category.name}</option>
+                                        {categories.length > 0 ?                                   
+                                            categories.map(c => (
+                                                <option key={c.id} value={c.id}>
+                                                    {c.name}
+                                                </option>
+                                            ))
+                                            :
+                                            null
+                                        }
+                                    </select>
+                                </div>
+                            </FormGroup>
+                            <FormGroup>
+                                <Label for="difficulty">Skill Level</Label><br></br>
+                                <div className="nes-select">
+                                    <select required id="difficulty" onChange={(e) => setDifficulty(e.target.value)}>
+                                        <option value={`${flashcardSet.difficultyId}`} disabled selected hidden>{flashcardSet.difficulty.name}</option>
+                                        {difficulties.length > 0 ?
+                                            difficulties.map(d => (
+                                                <option key={d.id} value={d.id}>
+                                                    {d.name}
+                                                </option>
+                                            ))
+                                            :
+                                            null
+                                        }
+                                    </select>
+                                </div>
+                            </FormGroup>
+                        </Form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary nes-btn" onClick={saveFlashcardEdit}>Save</Button>
+                        <Button color="secondary right nes-btn" onClick={toggleFlashcardModal}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
+
+                {/* Modal for question and answer edits */}
+                <Modal isOpen={qAndAModal} toggle={toggleQAndAModal} className="nes-dialog">
+                    <ModalHeader toggle={toggleQAndAModal}>Q/A Edit</ModalHeader>
                     <ModalBody>
                         <Form>
                                 <FormGroup>
@@ -160,10 +248,11 @@ const FlashCardEdit = () => {
                         </Form>
                     </ModalBody>
                     <ModalFooter>
-                        <Button color="primary nes-btn" onClick={saveEdit}>Save</Button>
-                        <Button color="secondary right nes-btn" onClick={toggleFlashcardModal}>Cancel</Button>
+                        <Button color="primary nes-btn" onClick={saveQAndAdEdit}>Save</Button>
+                        <Button color="secondary right nes-btn" onClick={toggleQAndAModal}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
+
         </div>
     )
 }
